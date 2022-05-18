2093cc3cfb4a01ac2529e2404359258040b25337
SteveDutch <steholl@posteo.org>
2022-05-18 10:40:22 +0200

    finalize SalesAnalyzer

--- src/com/coderscampus/assignment6/TeslaVehicleSalesAnalyzerApplication.java
+++ src/com/coderscampus/assignment6/TeslaVehicleSalesAnalyzerApplication.java
@@ -30,6 +30,7 @@ public class TeslaVehicleSalesAnalyzerApplication {
 		final String fileModelX = "modelX.csv";
 		FileParser report = new FileParser();
 		SalesAnalyzer aSalesAnalyzer = new SalesAnalyzer();
+		Printer printer = new Printer();
 		
 		//read in files
 		Map<YearMonth, Integer> model3 = report.readModel(fileModel3);
@@ -37,39 +38,15 @@ public class TeslaVehicleSalesAnalyzerApplication {
 		Map<YearMonth, Integer> modelX = report.readModel(fileModelX);
 
 		
-		
-	
-				
-		// TODO da ich dies 3x printen soll -> Methode, gell?
-		System.out.println("Model 3 Yearly Sales Report");
-		System.out.println("---------------------------");
-		System.out.println("2017 ->  " + aSalesAnalyzer.yearlySales(model3, 2017).getSum());
-		System.out.println("2018 ->  " + aSalesAnalyzer.yearlySales(model3, 2018).getSum());
-		System.out.println("2019 ->  " + aSalesAnalyzer.yearlySales(model3, 2019).getSum());
-		System.out.println("\nThe best month for Model 3 was: " + aSalesAnalyzer.getmonthlySalesMax(model3).get().getKey());
-		System.out.println("The worst month for Model 3 was: " +  aSalesAnalyzer.getmonthlySalesMin(model3).get().getKey() + "\n");
-		
-		System.out.println("Model S Yearly Sales Report");
-		System.out.println("---------------------------");
-		System.out.println("2017 ->  " + aSalesAnalyzer.yearlySales(modelS, 2016).getSum());
-		System.out.println("2017 ->  " + aSalesAnalyzer.yearlySales(modelS, 2017).getSum());
-		System.out.println("2018 ->  " + aSalesAnalyzer.yearlySales(modelS, 2018).getSum());
-		System.out.println("2019 ->  " + aSalesAnalyzer.yearlySales(modelS, 2019).getSum());
-		System.out.println("\nThe best month for Model S was: " + aSalesAnalyzer.getmonthlySalesMax(modelS).get().getKey());
-		System.out.println("The worst month for Model S was: " +  aSalesAnalyzer.getmonthlySalesMin(modelS).get().getKey() + "\n");
-		
-		System.out.println("Model X Yearly Sales Report");
-		System.out.println("---------------------------");
-		System.out.println("2016 ->  " + aSalesAnalyzer.yearlySales(modelX, 2016).getSum());
-		System.out.println("2017 ->  " + aSalesAnalyzer.yearlySales(modelX, 2017).getSum());
-		System.out.println("2018 ->  " + aSalesAnalyzer.yearlySales(modelX, 2018).getSum());
-		System.out.println("2019 ->  " + aSalesAnalyzer.yearlySales(modelX, 2019).getSum());
-		System.out.println("\nThe best month for Model X was: " + aSalesAnalyzer.getmonthlySalesMax(modelX).get().getKey());
-		System.out.println("The worst month for Model X was: " +  aSalesAnalyzer.getmonthlySalesMin(modelX).get().getKey() + "\n");
-		
+		// show reports
+		printer.showResultsThreeYears(aSalesAnalyzer, model3, "Model 3");
+		printer.showResults(aSalesAnalyzer, modelS, "Model S");
+		printer.showResults(aSalesAnalyzer, modelX, "Model X");
 		
 
 	}
+
+	
 
 	
 	
