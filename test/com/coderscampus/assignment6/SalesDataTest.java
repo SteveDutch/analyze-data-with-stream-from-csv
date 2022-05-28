3ba45a7f9c9a9519f3b0114fbc532b0710911fa2
SteveDutch <steholl@posteo.org>
2022-05-28 16:06:05 +0200

    test class, modify model3.csv
    Ziel: make code more flexible, regardless of year.
    -  years before 2016 & after 2019 should be detected, analyzed and
    printed out.

---
+++ test/com/coderscampus/assignment6/SalesDataTest.java
@@ -0,0 +1,14 @@
+package com.coderscampus.assignment6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SalesDataTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
